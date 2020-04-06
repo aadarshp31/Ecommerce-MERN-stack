e730036377c4dd4de334a8309dddbfb5895af6d6
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-04-06 23:46:57 +0530

    Initializing Github Repository

---
+++ projbackend/models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        maxlength = 32,
+        trim: true,
+        required: true
+    },
+    lastname: {
+        type: String,
+        maxlength: 32,
+        trim: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true
+    },
+    userinfo: {
+        type: String,
+        trim: true
+    },
+    //TODO come back here!
+    password: {
+        type: String,
+        trim: true
+    },
+    salt: String,
+    role: {
+        type: number,
+        default: 0
+    },
+    purchases: {
+        type: Array,
+        default: []
+    }
+});
+
+module.exports = mongoose.model("user", userSchema);
---
+++ testback/index.js
@@ -0,0 +1,34 @@
+const express  = require("express");
+const app = express();
+
+const port = 8000;
+
+app.get('/', (req, res) => {
+    return res.send("You're at Home");
+})
+
+app.get('/login', (req, res) => {
+    return res.send("You're at Login page");
+})
+
+app.get('/signup', (req, res) => {
+    return res.send("You're at signup page");
+})
+
+app.get('/signout', (req, res) => {
+    return res.send("You're at signout page");
+})
+
+app.get('/adarsh', (req, res) => {
+    return res.send("You're my creator!");
+})
+
+app.listen(port, () => {
+    console.log('Server is up and running...');
+})
+
+// const port = 3000;
+
+// app.get('/', (req,res) => res.send('Hello World!'));
+
+// app.listen(port, () => console.log(`Example app is listening at port: ${port}`));
