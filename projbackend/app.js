25bb4927eec7c1078bdee216000286fa77539120
Adarsh Pandey <aadarshp31@gmail.com>
2020-04-10 03:01:00 +0530

    Replacing DB path with Environment Variables

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,16 +1,17 @@
+require('dotenv').config()
 const mongoose = require("mongoose")
 const express = require("express")
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
-mongoose.connect("mongodb://localhost:27017/tshirt", {
+mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 }).then(() => {
     console.log("!!--------DB CONNECTED!--------!!");    
-})
+});
 
 app.listen(port, (req,res) => {
     console.log(`>> App is running at ${port}`);    
