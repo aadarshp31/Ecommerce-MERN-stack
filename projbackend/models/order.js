ce7fd663ee485d23869fb6cb70b8235dcd4d5471
Adarsh Pandey <aadarshp31@gmail.com>
2020-04-10 02:06:58 +0530

    Creating Order Model

---
+++ projbackend/models/order.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+const { ObjectId } = mongoose.Schema;
+
+const ProductCartSchema = new mongoose.Schema({
+    product: {
+        type: ObjectId,
+        ref: "Product"
+    },
+    name: String,
+    count: Number,
+    price: Number
+});
+
+const OrderSchema = new mongoose.Schema({
+    products: [ProductCartSchema],
+    transaction_id: {},
+    amount: Number,
+    address: String,
+    updated: Date,
+    user: {
+        type: ObjectId,
+        ref: "User"
+    }
+},
+{timestamps: true}
+);
+
+const ProductCart = mongoose.model("ProductCart", ProductCartSchema);
+const Order = mongoose.model("Order", OrderSchema);
+
+module.exports = {Order, ProductCart};
