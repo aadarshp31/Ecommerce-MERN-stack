fbaa0c7df24cb98fef170a3474a87588b43fa987
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-05-02 22:35:57 +0530

    Adding user signup process
    >Users data is stored in the database after validation done by express-validator as a middleware.
    
    >Custom message for each validation rule violation

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,5 +1,37 @@
-exports.signout = (req,res) => {
-    res.json({
-        message: "User Sign out page"
-    })
-}
+const User = require("../models/user")
+const { validationResult } = require("express-validator")
+
+exports.signup = (req, res) => {
+	//errors in an object containing the first element as array which contains the info related to the error
+	const errors = validationResult(req)
+
+	//Checking for errors thrown by express validator
+	if(!errors.isEmpty()){
+		return res.status(422).json({
+			error: `${errors.array()[0].msg}.\n Parameter: ${errors.array()[0].param}`		
+		})
+	}
+
+	//Creating new user for each signup request
+	const user = new User(req.body)
+	user.save((err, user) => {
+		//Checking for errors before saving to the database
+		if (err) {
+			return res.status(400).json({
+				err: "User was not stored in the database!"
+			});
+		}
+		//Response sent to the client after successfully saving the user signup data into the database
+		res.json({
+			id: user._id,
+			name: user.name,
+			email: user.email,
+		});
+	});
+};
+
+exports.signout = (req, res) => {
+	res.json({
+		message: "User Sign out page"
+	});
+};
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const router = express.Router()
-const { signout } = require("../controllers/auth")
+const { signup, signout } = require("../controllers/auth")
+const { check } = require("express-validator")
 
 //Routes
+router.post("/signup", [
+    check("name").isLength({ min: 2 }).withMessage("Name must be atleast 2 characters long"),
+    check("email").isEmail().withMessage("Email is required"),
+    check("password").isLength({ min: 6 }).withMessage("Password must be atleast 6 characters")
+], signup)
 router.get("/signout", signout)
 
 module.exports = router;
