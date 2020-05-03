439f56b8147cf708c19b64674891b15b79aefbe6
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-05-03 15:18:20 +0530

    Added Signin and Signout methods
    >Used jsonwebtoken from npm for token generation
    
    >Used express-jwt from npm as middleware for checking if the user is "Signedin" or not.

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require("../models/user")
 const { validationResult } = require("express-validator")
+const jwt = require("jsonwebtoken")
+const expressJwt = require("express-jwt")
 
 exports.signup = (req, res) => {
 	//errors in an object containing the first element as array which contains the info related to the error
@@ -22,6 +24,7 @@ exports.signup = (req, res) => {
 			});
 		}
 		//Response sent to the client after successfully saving the user signup data into the database
+		res.send(user)
 		res.json({
 			id: user._id,
 			name: user.name,
@@ -31,7 +34,56 @@ exports.signup = (req, res) => {
 };
 
 exports.signout = (req, res) => {
+	res.clearCookie()
 	res.json({
-		message: "User Sign out page"
+		message: "User Signed out successfully!"
 	});
 };
+
+
+exports.signin = (req, res) => {
+	//Destructuring email and password from the req.body
+	const { email, password } = req.body
+
+	//errors in an object containing the first element as array which contains the info related to the error
+	const errors = validationResult(req)
+
+	//Checking for errors thrown by express validator
+	if(!errors.isEmpty()){
+		return res.status(422).json({
+			error: `${errors.array()[0].msg}.\n Parameter: ${errors.array()[0].param}`		
+		})
+	}
+
+	User.findOne({ email }, (err, user) => {
+		if(err){
+			return err;
+		}
+		if(!user){
+			return res.status(400).json({
+				error: "User Email does not exists"
+			})
+		}
+		if(!user.authenticate(password)){
+			return res.status(401).json({
+				error: "Email and password do not match"
+			})
+		}
+
+		//Create authentication token
+		const token = jwt.sign({id: user._id}, process.env.SECRET);
+		//Put token in cookie
+		res.cookie("token", token, { expire: new Date() + 9999 });
+
+		//send response to the front end
+		const { _id, name, email, role } = user;
+		return res.json({ token, user: { _id, name, email, role }});
+	})
+
+}
+
+//Protected Routes
+exports.isSignedIn = expressJwt({
+	secret: process.env.SECRET,
+	userProperty: "auth"
+})
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,14 +1,31 @@
 const express = require("express")
 const router = express.Router()
-const { signup, signout } = require("../controllers/auth")
+const { signup, signout, signin, isSignedIn } = require("../controllers/auth")
 const { check } = require("express-validator")
 
 //Routes
+
+//Signup
 router.post("/signup", [
     check("name").isLength({ min: 2 }).withMessage("Name must be atleast 2 characters long"),
     check("email").isEmail().withMessage("Email is required"),
     check("password").isLength({ min: 6 }).withMessage("Password must be atleast 6 characters")
 ], signup)
+
+//Signout
 router.get("/signout", signout)
+
+//Signin
+router.post("/signin", [
+    check("email").isEmail().withMessage("Email is required"),
+    check("password").isLength({ min: 1 }).withMessage("Password Field is required")
+], signin)
+
+//Protected Test Route
+router.get("/testroute", isSignedIn, (req, res) => {
+    res.send("A protected route")
+    //This is for testing the auth object added to the req object
+    // res.json(req.auth)
+})
 
 module.exports = router;
