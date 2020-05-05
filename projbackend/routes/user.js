183b14887f1f23281f1dd07442cc0db9003b1564
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-05-05 16:06:48 +0530

    Added routes for user methods

---
+++ projbackend/routes/user.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const router = express.Router()
+
+const { getUserById, getUser, updateUser } = require("../controllers/user")
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth")
+
+//Params middleware
+router.param("userId", getUserById);
+
+//Protected routes
+router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
+router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+
+module.exports = router;
