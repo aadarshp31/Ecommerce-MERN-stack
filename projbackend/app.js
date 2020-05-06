a6019a948b5c51ef59500f0908a88a3eca887d99
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-05-06 23:15:49 +0530

    Added category CRUD methods and routes
    >Added CRUD methods
    >CRUD methods are limited to only admin users, normal users can't execute these methods
    
    >Normal user can execute these methods
    >getCategoryById
    >getCategory
    >getAllCategories

--- projbackend/app.js
+++ projbackend/app.js
@@ -10,6 +10,7 @@ const cors = require("cors")
 //My Routes
 const authRoutes = require("./routes/auth")
 const userRoutes = require("./routes/user")
+const categoryRoutes = require("./routes/category")
 
 //DB Connection
 mongoose.connect(process.env.DATABASE, {
@@ -28,6 +29,7 @@ app.use(cors());
 //My Routes middlewares
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
 
 //PORT
 const port = process.env.PORT;
---
+++ projbackend/routes/category.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const router = express.Router();
+const { getCategoryById, createCategory, getCategory, getAllCategory, updateCategory, removeCategory } = require("../controllers/category")
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth")
+const { getUserById } = require("../controllers/user")
+
+//Param middleware
+router.param("userId", getUserById)
+router.param("categoryId", getCategoryById)
+
+//Create category route
+router.post("/category/create/:userId", isSignedIn, isAuthenticated, isAdmin, createCategory)
+
+//Get category routes
+router.get("/category/:categoryId", getCategory);
+router.get("/categories", getAllCategory);
+
+
+//Update category routes
+router.put("/category/:categoryId/:userId", isSignedIn, isAuthenticated, isAdmin, updateCategory)
+
+//Delete category routes
+router.delete("/category/:categoryId/:userId", isSignedIn, isAuthenticated, isAdmin, removeCategory)
+
+//exporting all routes to express.Router()
+module.exports = router;
