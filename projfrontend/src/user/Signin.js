657ca9679c7aa06901479828870da311088e36d9
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-05-16 22:48:56 +0530

    Created Signin and Signout components
    > Created using Base component with Props to customise the Base component elements

---
+++ projfrontend/src/user/Signin.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
+import Base from "../core/Base"
+
+const Signin = () => {
+    return(
+        <div>
+            <Base title="Signin Page" description="A page for the user to Sign in!">
+            </Base>
+        </div>
+    );
+}
+
+
+export default Signin;
---
+++ projfrontend/src/user/Signup.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
+import Base from "../core/Base"
+
+const Signup = () => {
+    return(
+        <div>
+            <Base title="Signup Page" description="A page for the user to Sign up!">
+            </Base>
+        </div>
+    );
+}
+
+
+export default Signup;
