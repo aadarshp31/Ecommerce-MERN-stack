23569cd1a3dc0acc654340c1452730885605c8a5
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-05-16 22:47:17 +0530

    Update Home.js
    > Created a simple layout for the home page using Base component

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,14 +1,26 @@
 import React from "react";
 import {API} from "../backend";
+import "../styles.css"
+import Base from "../core/Base"
 
 
 console.log("Environment Variable:", API);
 
 const Home = () => {
     return(
-        <div>
-            <h1>This is Home Component</h1>
-        </div>
+        <Base title="Home Page" description="Welcome to the Tshirt Store" >
+            <div className="row">
+                <div className="col-4">
+                    <button className="btn btn-success">Test Button</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn btn-success">Test Button</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn btn-success">Test Button</button>
+                </div>
+            </div>
+        </Base>
     );
 }
 
