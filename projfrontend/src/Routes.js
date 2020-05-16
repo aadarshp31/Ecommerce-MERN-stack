3ba99ef636358312b175bc391b3326836964e954
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-05-16 22:50:23 +0530

    Update Routes.js
    > Added Signin and Signout routes to load Signin and Signout components when their respective routes are hit.

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -1,14 +1,17 @@
 import React from "react";
 import {Route, Switch, BrowserRouter as Router} from "react-router-dom";
 import Home from "./core/Home"
-import App from "./App"
+import "./core/Menu"
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
 
 const Routes = () => {
     return(
         <Router>
             <Switch>
                 <Route exact path="/" component={Home} />
-                <Route path="/hi" component={App} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/signin" component={Signin} />
             </Switch>
         </Router>
     );
