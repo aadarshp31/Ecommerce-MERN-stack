2bf8911c166fb47baa6348d7fb72b9f0c6bc2778
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-05-16 22:41:03 +0530

    Created a Base component
    > Base component is created to be used all accross the website.
    > The Base component is made highly reusable via props such as "title" and "description" etc.
    > Made using Bootstap css framework

---
+++ projfrontend/src/core/Base.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Menu from "./Menu"
+
+const Base = ({
+	title = "My Title",
+	description = "My Description",
+	className = "bg-dark text-white text-center p-4 display-4",
+	children,
+}) => {
+	return (
+		<div>
+        <Menu />
+			<div className="container-fluid">
+				<div className="jumbotron bg-dark text-white text-center">
+					<h2 className="display-4">{title}</h2>
+					<p className="lead">{description}</p>
+				</div>
+				<div className={className}>{children}</div>
+			</div>
+			<footer className="footer bg-dark mt-auto py-3">
+				<div className="container-fluid bg-success text-center text-white py-3">
+					<h4>If you've got any questions, feel free to reach out!</h4>
+					<button className="btn btn-warning btn-lg">Contact Us</button>
+				</div>
+				<div className="container">
+					<span className="text-muted">
+						An amazing <span className="text-white">MERN</span> Stack Project
+					</span>
+				</div>
+			</footer>
+		</div>
+	);
+};
+
+export default Base;
