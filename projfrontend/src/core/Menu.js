244666866cbe2ac8bf8e646d330b32598321fb62
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-05-19 23:03:29 +0530

    Update Menu.js
    > Added feature to hide Signin tab in navbar when user is authenticated.
    > Added feature to hide Signout tab in navbar when user is not authenticated.
    > After signout, user is redirected to the home page.
    > After signin, user is shown message showing to which page the user shall be redirected.
    > Signout tab in navbar is shown in different color to standout from the rest.

--- projfrontend/src/core/Menu.js
+++ projfrontend/src/core/Menu.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import { signout, isAuthenticated } from "../auth/helper/index";
 
 const currentTab = (history, path) => {
-    if(history.location.pathname === path){
-        return {color: "#2ecc72"};
-    } else {
-        return {color: "#ffffff"};
-    }
-}
+	if (history.location.pathname === path) {
+		return { color: "#2ecc72" };
+	} else {
+		return { color: "#ffffff" };
+	}
+};
 
 const Menu = (history) => (
 	<div>
@@ -18,35 +19,73 @@ const Menu = (history) => (
 				</Link>
 			</li>
 			<li className="nav-item">
-				<Link style={currentTab(history, "/cart")} className="nav-link" to="/cart">
+				<Link
+					style={currentTab(history, "/cart")}
+					className="nav-link"
+					to="/cart"
+				>
 					Cart
 				</Link>
 			</li>
 			<li className="nav-item">
-				<Link style={currentTab(history, "/user/dashboard")} className="nav-link" to="/user/dashboard">
+				<Link
+					style={currentTab(history, "/user/dashboard")}
+					className="nav-link"
+					to="/user/dashboard"
+				>
 					Dashboard
 				</Link>
 			</li>
 			<li className="nav-item">
-				<Link style={currentTab(history, "/admin/dashboard")} className="nav-link" to="/admin/dashboard">
+				<Link
+					style={currentTab(history, "/admin/dashboard")}
+					className="nav-link"
+					to="/admin/dashboard"
+				>
 					Admin Dashboard
 				</Link>
 			</li>
 			<li className="nav-item">
-				<Link style={currentTab(history, "/signup")} className="nav-link" to="/signup">
+				<Link
+					style={currentTab(history, "/signup")}
+					className="nav-link"
+					to="/signup"
+				>
 					Signup
 				</Link>
 			</li>
-			<li className="nav-item">
-				<Link style={currentTab(history, "/signin")} className="nav-link" to="/signin">
-					Signin
-				</Link>
-			</li>
-			<li className="nav-item">
-				<Link style={currentTab(history, "/signout")} className="nav-link" to="/signout">
-					Signout
-				</Link>
-			</li>
+			{/* Returns Signin nav button when user is not authenticated, otherwise hides it from navbar */}
+			{!isAuthenticated() && (
+				<li className="nav-item">
+					<Link
+						style={currentTab(history, "/signin")}
+						className="nav-link"
+						to="/signin"
+					>
+						Signin
+					</Link>
+				</li>
+			)}
+			{/* Returns Signout nav button when user is authenticated, otherwise hides it from navbar */}
+			{/* onCLick event fires a callback to initiate "signout" method which fires callback to redirect the user to "/" */}
+			{isAuthenticated() && (
+				<li
+					className="nav-item"
+					onClick={() => {
+						signout(() => {
+							history.history.push("/");
+						});
+					}}
+				>
+					<Link
+						style={currentTab(history, "/signout")}
+						className="nav-link text-warning"
+						to=""
+					>
+						Signout
+					</Link>
+				</li>
+			)}
 		</ul>
 	</div>
 );
