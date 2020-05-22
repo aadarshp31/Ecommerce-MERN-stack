cda6439715b5c280be6a10e6d7853b1578c2ee3a
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-05-22 23:58:31 +0530

    Created Add Category Component

---
+++ projfrontend/src/admin/AddCategory.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddCategory = () => {
+	return (
+		<div>
+			<h4 className="text-white">Add/Create Category here...</h4>
+		</div>
+	);
+};
+
+export default AddCategory;
