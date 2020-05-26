3d115061697f7c14756a450069d48cda178ee56c
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-05-26 21:14:04 +0530

    Create ManageCategories.js
    > Created ManageCategories page for managing all categories for the product.

---
+++ projfrontend/src/admin/ManageCategories.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Base from "../core/Base";
+import { getAllCategories } from "../admin/helper/adminapicall";
+
+const ManageCategories = () => {
+	return (
+		<Base
+			title="Manage Categories"
+			description="Manage all Product Categories here"
+		>
+		</Base>
+	);
+};
+
+export default ManageCategories;
