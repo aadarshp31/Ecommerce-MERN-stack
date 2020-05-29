cf2da7bd256df22381541fd39d2bfb17598dbade
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-05-29 16:16:32 +0530

    Create UpdateCategory.js
    > This component contains Delete and Update operations for all the categories which are fetched from the backend.
    > Added status messages such as - loading, success, redirecting, error messages.
    > After update is successfull, the page will show a redirecting message and redirect the user after waitng 2seconds.

---
+++ projfrontend/src/admin/UpdateCategory.js
@@ -0,0 +1,161 @@
+import React, { useState, useEffect } from "react";
+import Base from "../core/Base";
+import { Link } from "react-router-dom";
+import { isAuthenticated } from "../auth/helper/index";
+import { getCategory, updateCategory } from "./helper/adminapicall";
+
+const UpdateCategory = ({match, history}) => {
+	const [name, setName] = useState("");
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [success, setSuccess] = useState(false);
+	const [didRedirect, setDidRedirect] = useState(false);
+
+	//Getting token and user information from localStorage of client's browser
+	const { token, user } = isAuthenticated();
+
+	const goBackButton = () => {
+		return (
+			<div>
+				<Link className="btn btn-sm btn-success mb-3" to="/admin/dashboard">
+					Admin Dashboard
+				</Link>
+			</div>
+		);
+	};
+
+	//Sets data in the states according to the input fields
+	const handleChange = (event) => {
+		setName(event.target.value);
+		setError(false);
+		setSuccess(false);
+    };
+
+    const preload = (categoryId) => {
+        setLoading(true);
+        getCategory(categoryId)
+        .then(data => {
+            if(data.error){
+                setError(data.error);
+            } else {
+                setName(data.name);
+                setLoading(false);
+            }
+        })
+        .catch(err => console.log(err));
+    }
+
+    useEffect(() => {
+        preload(match.params.categoryId);
+    }, [])
+
+	//Submits the UpdateCatgory form and gets the response from the backend
+	const formSubmit = (event) => {
+        event.preventDefault();
+		setError("");
+        setLoading(true);
+		setSuccess(false);
+
+		//Backend request fired
+		updateCategory(user._id, match.params.categoryId, token, { name })
+			.then((data) => {
+				if (data.error) {
+					setError(data.error);
+				} else {
+                    setError("");
+                    setLoading(false);
+					setSuccess(true);
+                    setDidRedirect(true);
+				}
+			})
+			.catch((err) => console.log("Error in formSubmit!", err));
+	};
+
+	//Success message popup
+	const successMessage = () => {
+		if (success) {
+			return <h4 className="text-success">Collection Updated Successfully</h4>;
+		}
+	};
+
+	//Error message popup
+	const errorMessage = () => {
+		if (error) {
+			return (
+				<div className="text-danger">
+					<h4>Collection Updation Failed!</h4>
+					<p>{error}</p>
+				</div>
+			);
+		}
+    };
+    
+	const loadingMessage = () => {
+		if (loading) {
+			return <h4 className="text-info">Loading...</h4>;
+		}
+    };
+    
+    const redirectingMessage = () => {
+		if (didRedirect) {
+			return (
+				<div className="text-info">
+					<h4 className="text-info">Redirecting to Manage Categories Page...</h4>
+				</div>
+			);
+		}
+	};
+
+    const performRedirect = () => {
+		if (didRedirect) {
+			setTimeout(() => {
+				history.push("/admin/categories");
+			}, 2000);
+		}
+	};
+
+	//Update Category form component
+	const addCategoryForm = () => {
+		return (
+			<form>
+				<div className="form-group">
+					<label className="lead">Enter Category Name</label>
+					<input
+						type="text"
+						className="form-control my-3"
+						autoFocus
+						placeholder="For Ex. Summer"
+						onChange={handleChange}
+						value={name}
+						required
+					/>
+					<button onClick={formSubmit} className="btn btn-info">
+						Update Category
+					</button>
+				</div>
+			</form>
+		);
+	};
+
+	return (
+		<Base
+			title="Update a Category"
+			description="Update a product category for TShirts"
+			className="container bg-white rounded p-4"
+		>
+			<div className="row rounded">
+				<div className="col-md-2">{goBackButton()}</div>
+				<div className="col-md-8 my-3">
+					{addCategoryForm()}
+					{successMessage()}
+					{errorMessage()}
+                    {loadingMessage()}
+                    {redirectingMessage()}
+                    {performRedirect()}
+				</div>
+			</div>
+		</Base>
+	);
+};
+
+export default UpdateCategory;
