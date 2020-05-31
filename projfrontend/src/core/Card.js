80fce3ffbbcc0210187f69bc6b342a0c1350b095
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-05-31 22:57:28 +0530

    Added functionality for conditional rendering of the buttons on card.

--- projfrontend/src/core/Card.js
+++ projfrontend/src/core/Card.js
@@ -1,7 +1,24 @@
 import React from "react";
 import ImageHelper from "./helper/ImageHelper";
 
-const Card = ({product}) => {
+const Card = ({product, addToCart=true, removeFromCart=false}) => {
+    const showAddToCart = (addToCart) => {
+        return(
+            addToCart && (
+                <a href="#" className="btn btn-info btn-block rounded">Add to Cart</a>
+            )
+        );
+    }
+
+    const showRemoveFromCart = (removeFromCart) => {
+        return(
+            removeFromCart && (
+                <a href="#" className="btn btn-danger btn-block rounded">Remove from Cart</a>
+            )
+        );
+    }
+
+
     return(
         <div className="card text-secondary mx-auto" style={{"width": "18rem"}}>
             <ImageHelper product={product} />
@@ -9,8 +26,8 @@ const Card = ({product}) => {
                 <h5 className="card-title">Card title</h5>
                 <p className="badge badge-secondary px-3 py-2">₹ 999</p>                
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-info btn-block rounded">Add to Cart</a>
-                <a href="#" className="btn btn-danger btn-block rounded">Remove from Cart</a>
+                {showAddToCart(addToCart)}
+                {showRemoveFromCart(removeFromCart=true)}
             </div>
         </div>
     );
