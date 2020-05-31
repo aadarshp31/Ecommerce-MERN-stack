cd2e74105e5517f6efb88e7928b083e1850ce6b1
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-05-31 23:44:21 +0530

    Loading all products from the backend.
    > We are fetching all the products from the backend and rendering equal number of cards for them.

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles.css";
 import Base from "../core/Base";
 import Card from "./Card";
-
+import { getProducts } from "./helper/coreapicalls"
 
 const Home = () => {
+	const [products, setProducts] = useState([]);
+	const [error, setError] = useState(false);
+	
+	const loadAllProducts = () => {
+		return getProducts()
+		.then(data => {
+			if(data.error){
+				setError(data.error);
+			} else {
+				setProducts(data);
+			}
+		})
+		.catch(err => console.log(err));
+	}
+
+	useEffect(() => {
+		loadAllProducts();
+	}, [])
+
+
 	return (
 		<Base title="Home Page" description="Welcome to the Tshirt Store">
-			<div className="row">
-                <div className="col-4">
-                    <Card />
-                </div>
-				<div className="col-4">
-                    <Card />
+                <h1 className="text-white mx-auto mb-5">All T-Shirts</h1>
+				<div className="row">
+					{products.map((product, index) => {
+						return(
+							<div key={index} className="col-4 mb-4">
+								<Card removeFromCart="true" />
+							</div>
+						);
+					})}
 				</div>
-				<div className="col-4">
-                    <Card />
-				</div>
-			</div>
 		</Base>
 	);
 };
