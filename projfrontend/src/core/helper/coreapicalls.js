d4f8f57866b5ce2771462ba46b070e5d231273e8
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-05-31 23:41:38 +0530

    Update coreapicalls.js
    > Typo mistake in path.

--- projfrontend/src/core/helper/coreapicalls.js
+++ projfrontend/src/core/helper/coreapicalls.js
@@ -1,7 +1,7 @@
 import { API } from "../../backend";
 
 export const getProducts = () => {
-    return fetch(`${API}.products`,{method: "GET"})
+    return fetch(`${API}/products`,{method: "GET"})
     .then(response => response.json())
     .catch(err => console.log(err));
 }
