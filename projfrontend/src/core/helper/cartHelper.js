0f0baf323d3f78ff2cc00e96c969bd0a52001135
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-06-01 22:23:37 +0530

    Update cartHelper.js
    > Created method to remove items from localStorage .
    > Created method to load all products from the localStorage to the Cart Page.

--- projfrontend/src/core/helper/cartHelper.js
+++ projfrontend/src/core/helper/cartHelper.js
@@ -11,3 +11,23 @@ export const addItemToCart = (item, next) => {
         next();
     }
 };
+
+export const loadCart = () => {
+    if(typeof window !== undefined) {
+        if(localStorage.getItem("cart")) {
+            return JSON.parse(localStorage.getItem("cart"));
+        }
+    }
+};
+
+export const removeItemFromCart = (productId) => {
+    let cart = [];
+    if(typeof window !== undefined) {
+        if(localStorage.getItem("cart")) {
+            cart = JSON.parse(localStorage.getItem("cart"));
+            cart = cart.filter((product) => product._id !== productId );
+            localStorage.setItem("cart", JSON.stringify(cart));
+        }
+        return cart;
+    }
+}
