baccf9b97b74baffff7795ddc5066f07c350dbe2
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2020-06-02 19:49:26 +0530

    Update StripeCheckout.js
    > Added method to calculate total amount for the products in cart.
    > Added method for conditional loading of  Pay with Stripe button depending on the user is signed in or not.

--- projfrontend/src/core/StripeCheckout.js
+++ projfrontend/src/core/StripeCheckout.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { isAuthenticated } from "../auth/helper";
+import { Link } from "react-router-dom";
 
 const StripeCheckout = ({ products, setReload = (f) => f, reload=undefined }) => {
     const initialValues = {
@@ -10,12 +11,28 @@ const StripeCheckout = ({ products, setReload = (f) => f, reload=undefined }) =>
     }
     const [data, setData] = useState(initialValues);
     const { user, token } = isAuthenticated();
-
+    
+    const getFinalAmount = () => {
+        let amount = 0;
+        products.map(p => {
+            amount += p.price
+        });
+        return amount;
+    }
+    const showStripeButton = () => {
+        return isAuthenticated() ? (
+            <button className="btn btn-info rounded">Pay with Stripe</button>
+        ) : (
+            <Link to="/signin">
+                <button className="btn btn-warning rounded">Signin</button>
+            </Link>
+        );
+    }
 
     return(
         <div>
-            <h2 className="text-white">Stripe Checkout Loaded!</h2>
-
+            <h2 className="text-white">Stripe Checkout Loaded! Amount: {getFinalAmount()}</h2>
+            {showStripeButton()}
         </div>
     );
 }
