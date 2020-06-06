ff5b11e11a580b7faa8a1b4236831abc66e1f9f8
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-06 19:51:50 +0530

    Added  stripe route and controller to the backend API route using express middleware

--- projbackend/app.js
+++ projbackend/app.js
@@ -13,6 +13,7 @@ const userRoutes = require("./routes/user")
 const categoryRoutes = require("./routes/category")
 const productRoutes = require("./routes/product")
 const orderRoutes = require("./routes/order")
+const stripeRoutes = require("./routes/stripePayment")
 
 //DB Connection
 mongoose.connect(process.env.DATABASE, {
@@ -34,6 +35,7 @@ app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
 app.use("/api", orderRoutes);
+app.use("/api", stripeRoutes);
 
 //PORT
 const port = process.env.PORT;
