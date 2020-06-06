8ef97555a4a4f63ef31ee92b19e3a6a47d6c72d3
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-06 23:47:28 +0530

    Removed protected routes due to bugs during testing payment.

--- projbackend/routes/stripePayment.js
+++ projbackend/routes/stripePayment.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { makePayment } = require("../controllers/stripePayment");
-const { isSignedIn, isAuthenticated } = require("../controllers/auth");
 
-router.post("/payment", isSignedIn, isAuthenticated, makePayment);
+router.post("/payment", makePayment);
 
 module.exports = router;
