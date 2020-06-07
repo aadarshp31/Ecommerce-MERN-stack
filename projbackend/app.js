1302baa2344f82ee4efa4907dc710e9f9c138a39
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-07 23:24:43 +0530

    Added the routes for handling Paypal payments.

--- projbackend/app.js
+++ projbackend/app.js
@@ -14,6 +14,7 @@ const categoryRoutes = require("./routes/category")
 const productRoutes = require("./routes/product")
 const orderRoutes = require("./routes/order")
 const stripeRoutes = require("./routes/stripePayment")
+const paypalRoutes = require("./routes/paypalRoutes")
 
 //DB Connection
 mongoose.connect(process.env.DATABASE, {
@@ -36,6 +37,7 @@ app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
 app.use("/api", orderRoutes);
 app.use("/api", stripeRoutes);
+app.use("/api", paypalRoutes);
 
 //PORT
 const port = process.env.PORT;
