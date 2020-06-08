5397455a7924b73cc3cfe8efc2364465c04d1582
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-08 20:46:45 +0530

    Configured controllers to handle payments with paypal
    Added methods to generate token and send it to the frontend.
    Added method to generate transaction with the nonce and amount recieved from the frontend/client.
    Configured the Environment sandbox for braintree with variables: merchantId, publicKey and privateKey using environment variables.

---
+++ projbackend/controllers/paypalPayment.js
@@ -0,0 +1,42 @@
+var braintree = require("braintree");
+
+var gateway = braintree.connect({
+	environment: braintree.Environment.Sandbox,
+	merchantId: process.env.BRAINTREE_MERCHENT_ID,
+	publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+	privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+});
+
+//Generate Token and send it to the client/frontend application
+exports.getToken = (req, res) => {
+	gateway.clientToken.generate({}, function (err, response) {
+		if (err) {
+			res.status(500).json(err);
+		} else {
+			res.send(response);
+		}
+	});
+};
+
+//Recieve 'paymentMethodNonce' & 'amount' from frontend and create transaction for the payment.
+exports.processPayment = (req, res) => {
+	let nonceFromTheClient = req.body.paymentMethodNonce;
+	let amountFromTheClient = req.body.amount;
+
+	gateway.transaction.sale(
+		{
+			amount: amountFromTheClient,
+			paymentMethodNonce: nonceFromTheClient,
+			options: {
+				submitForSettlement: true,
+			},
+		},
+		function (err, result) {
+			if (err) {
+				res.status(500).json(err);
+			} else {
+				res.send(result);
+			}
+		}
+	);
+};
