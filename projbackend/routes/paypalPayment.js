b5bf4643f569c2ff44a420965f96246c8711baa3
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-09 22:43:58 +0530

    Added Authentication check using param

--- projbackend/routes/paypalPayment.js
+++ projbackend/routes/paypalPayment.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { isSignedIn, isAuthenticated } = require("../controllers/auth");
 const { getToken, processPayment } = require("../controllers/paypalPayment");
+const { getUserById } = require("../controllers/user");
+
+router.param("userId", getUserById);
 
 router.get("/payment/gettoken/:userId", isSignedIn, isAuthenticated, getToken)
 router.post("/payment/paypal/:userId", isSignedIn, isAuthenticated, processPayment)
