253cca474d4ced7c93508bf5ea4ab4c2ac0e4189
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-09 23:25:41 +0530

    Added conditional loading of 'PaypalCheckout' component

--- projfrontend/src/core/PaypalCheckout.js
+++ projfrontend/src/core/PaypalCheckout.js
@@ -1,9 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import DropIn from "braintree-web-drop-in-react";
+import { loadCart, clearCart } from "./helper/cartHelper";
+import { isAuthenticated } from "../auth/helper";
+import { Link } from "react-router-dom";
+import { getToken, processPayment } from "./helper/paypalPaymentHelper";
 
-const PaypalCheckout = () => {
+const PaypalCheckout = ({
+	products,
+	setReload = (f) => f,
+	reload = undefined,
+}) => {
+	const initialValues = {
+		loading: false,
+		success: false,
+		clientToken: null,
+		instance: {},
+		error: "",
+	};
+    //States
+	const [info, setInfo] = useState(initialValues);
+    //User data
+	const { user, token } = isAuthenticated();
+
+	//Getting clientToken from the backend
+	const getClientToken = (userId, token) => {
+		getToken(userId, token)
+			.then((data) => {
+				if (data.error) {
+					setInfo({ error: data.error });
+				} else {
+					const clientToken = data.clientToken;
+					setInfo({clientToken });
+				}
+			})
+			.catch((err) => console.log(err));
+	};
+
+	useEffect(() => {
+        if (isAuthenticated()) {
+            getClientToken(user._id, token);
+        }
+	}, []);
+
+    //Conditional loading of Paypal Checkout
+	const showPaypalCheckout = () => {
+		return info.clientToken !== null && isAuthenticated() && products.length > 0 ? (
+			<div>
+				<DropIn
+					options={{ authorization: info.clientToken }}
+					onInstance={instance => (info.instance = instance)}
+				/>
+				<button onClick={() => {}} className="btn btn-info rounded">Pay with Paypal</button>
+			</div>
+		) : (
+			<div>
+				{!isAuthenticated() ? (
+				<Link to="/signin">
+					<button className="btn btn-warning rounded">Signin</button>
+				</Link>
+				) : (
+                    <h5>The cart is empty...</h5>
+                )}
+			</div>
+		);
+	};
+
 	return (
 		<div>
 			<h4>Paypal Checkout</h4>
+            {showPaypalCheckout()}
 		</div>
 	);
 };
