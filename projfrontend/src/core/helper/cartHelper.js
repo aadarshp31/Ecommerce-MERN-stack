b1141d9c128f458cf7bfffacea3e4ad742115270
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-12 19:53:15 +0530

    Added a method to update cart product
    Updates product stored in localStorage of client's browser.

--- projfrontend/src/core/helper/cartHelper.js
+++ projfrontend/src/core/helper/cartHelper.js
@@ -1,55 +1,68 @@
 //Create and save cart array in the localStorage of the browser if not present already.
 export const createCart = () => {
-    if(typeof window !== undefined) {
-        if(!localStorage.getItem("cart")) {
-            let cart = [];
-            localStorage.setItem("cart", JSON.stringify(cart));
-        }
-    }
-}
+	if (typeof window !== undefined) {
+		if (!localStorage.getItem("cart")) {
+			let cart = [];
+			localStorage.setItem("cart", JSON.stringify(cart));
+		}
+	}
+};
 
+//Create
 export const addItemToCart = (item, next) => {
-    let cart = [];
-    if(typeof window !== undefined){
-        if(localStorage.getItem("cart")) {
-            cart = JSON.parse(localStorage.getItem("cart"));
-        }
-        cart.push({
-            ...item
-        });
-        localStorage.setItem("cart", JSON.stringify(cart));
-        next();
-    }
+	let cart = [];
+	if (typeof window !== undefined) {
+		if (localStorage.getItem("cart")) {
+			cart = JSON.parse(localStorage.getItem("cart"));
+		}
+		cart.push({
+			...item,
+		});
+		localStorage.setItem("cart", JSON.stringify(cart));
+		next();
+	}
 };
 
+//Read
 export const loadCart = () => {
-    if(typeof window !== undefined) {
-        if(localStorage.getItem("cart")) {
-            return JSON.parse(localStorage.getItem("cart"));
-        }
-    }
+	if (typeof window !== undefined) {
+		if (localStorage.getItem("cart")) {
+			return JSON.parse(localStorage.getItem("cart"));
+		}
+	}
 };
 
-export const removeItemFromCart = (productId) => {
-    let cart = [];
-    if(typeof window !== undefined) {
-        if(localStorage.getItem("cart")) {
-            cart = JSON.parse(localStorage.getItem("cart"));
-            cart = cart.filter((product) => product._id !== productId );
-            localStorage.setItem("cart", JSON.stringify(cart));
-        }
-        return cart;
-    }
-}
+//Update
+export const updateCart = (updatedCart) => {
+	if (typeof window !== undefined) {
+		if (localStorage.getItem("cart")) {
+			localStorage.setItem("cart", JSON.stringify(updatedCart));
+		}
+	}
+};
 
+//Delete
+export const removeItemFromCart = (productId) => {
+	let cart = [];
+	if (typeof window !== undefined) {
+		if (localStorage.getItem("cart")) {
+			cart = JSON.parse(localStorage.getItem("cart"));
+			cart = cart.filter((product) => product._id !== productId);
+			localStorage.setItem("cart", JSON.stringify(cart));
+		}
+		return cart;
+	}
+};
+
+//Empty Cart
 export const clearCart = (next) => {
-    if(typeof window !== undefined) {
-        if(localStorage.getItem("cart")) {
-            localStorage.removeItem("cart");
-            //Add an empty cart array to avoid any error
-            let cart = [];
-            localStorage.setItem("cart", JSON.stringify(cart));
-        }
-    }
-    next();
-}
+	if (typeof window !== undefined) {
+		if (localStorage.getItem("cart")) {
+			localStorage.removeItem("cart");
+			//Add an empty cart array to avoid any error
+			let cart = [];
+			localStorage.setItem("cart", JSON.stringify(cart));
+		}
+	}
+	next();
+};
