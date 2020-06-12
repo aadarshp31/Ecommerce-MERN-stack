68fa3682b4a22c540a4caf1663e5226a48ff5b69
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-12 23:14:54 +0530

    Feature: getQuantity info on home screen
    Added a new method called 'getQuantityFromCart'.
    This method fetches quantity info from the cart of localStorage and updates the state 'products' on the homepage.

--- projfrontend/src/core/helper/cartHelper.js
+++ projfrontend/src/core/helper/cartHelper.js
@@ -32,6 +32,17 @@ export const loadCart = () => {
 	}
 };
 
+export const getQuantityFromCart = (products) => {
+    let cart = loadCart();
+    cart.map(item => {
+        products.map(product => {
+            if (item._id === product._id) {
+                product.quantity = item.quantity;
+            }
+        })
+    })
+}
+
 //Update
 export const updateCart = (updatedCart) => {
 	if (typeof window !== undefined) {
