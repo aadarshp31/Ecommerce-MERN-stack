566cc04bcff74247ecc150c4d7139db8004cd0ac
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-12 20:10:44 +0530

    Feature: Added 'Quantity' component in cart product cards
    Added showQuantity component method to display quantity info when product is added to cart.
    Conditional loading of 'Quantity' and 'Add to Cart' components in cart.
    If a product is already available in cart then only it's 'quantity' parameter changes upon pressing 'Add to Cart' button.
    Avoids duplication of products in cart.
    '+' and '-' buttons on card changes the quantity state of the product and saves it in the localStorage.

--- projfrontend/src/core/Card.js
+++ projfrontend/src/core/Card.js
@@ -1,61 +1,157 @@
 import React, { useState } from "react";
 import ImageHelper from "./helper/ImageHelper";
-import { addItemToCart, removeItemFromCart } from "./helper/cartHelper"
+import {
+	addItemToCart,
+	removeItemFromCart,
+	updateCart,
+	loadCart,
+} from "./helper/cartHelper";
 import { Redirect } from "react-router-dom";
 
-const Card = ({product, addToCartButton=true, removeFromCartButton=false, setReload= (f) => f, reload=undefined}) => {
-    const cardTitle = product ? product.name : "A Photo from Pexels";
-    const cardDescription = product ? product.description : "Product Description";
-    const cardPrice = product ? product.price : "DEFAULT";
+const Card = ({
+	product,
+	addToCartButton = true,
+	removeFromCartButton = false,
+	setReload = (f) => f,
+	reload = undefined,
+}) => {
+	const cardTitle = product ? product.name : "A Photo from Pexels";
+	const cardDescription = product ? product.description : "Product Description";
+	const cardPrice = product ? product.price : "DEFAULT";
 
-    const [redirect, setRedirect] = useState(false)
+	const [redirect, setRedirect] = useState(false);
 
-    const addToCart = () => { 
-        product.quantity = 1;
-        addItemToCart(product, () => setRedirect(true));
-        setReload(!reload);
-    }
-    const removeFromCart = () => { 
-        removeItemFromCart(product._id);
-        setReload(!reload);
-    }
+	const addToCart = () => {
+		let cart = loadCart();
 
-    const performRedirect = (redirect) => {
-        if(redirect) {
-            return <Redirect to="/cart" />;
-        }
-    }
+		//For empty Cart
+		if (cart.length === 0) {
+			product.quantity = 1;
+			addItemToCart(product, () => setRedirect(true));
+			//For non-empty Cart
+		} else {
+			let checkDuplicate = cart.filter((item) => item._id === product._id);
+			//For duplicate Product
+			if (checkDuplicate.length) {
+				cart.map((item) => {
+					if (item._id === product._id) {
+						incQuantity();
+					}
+				});
+				//For non-duplicate Product
+			} else {
+				product.quantity = 1;
+				addItemToCart(product, () => setRedirect(true));
+			}
+		}
+		setReload(!reload);
+	};
+	const removeFromCart = () => {
+		removeItemFromCart(product._id);
+		setReload(!reload);
+	};
 
-    const showaddToCartButton = (addToCartButton) => {
-        return(
-            addToCartButton && (
-                <button className="btn btn-info btn-block rounded" onClick={addToCart}>Add to Cart</button>
-            )
-        );
-    }
+	const performRedirect = (redirect) => {
+		if (redirect) {
+			return <Redirect to="/cart" />;
+		}
+	};
 
-    const showremoveFromCartButton = (removeFromCartButton) => {
-        return(
-            removeFromCartButton && (
-                <button className="btn btn-danger btn-block rounded" onClick={removeFromCart}>Remove from Cart</button>
-            )
-        );
-    }
+	const showaddToCartButton = (addToCartButton) => {
+		return (
+			addToCartButton && (
+				<button className="btn btn-info btn-block rounded" onClick={addToCart}>
+					Add to Cart
+				</button>
+			)
+		);
+	};
 
+	const showremoveFromCartButton = (removeFromCartButton) => {
+		return (
+			removeFromCartButton && (
+				<button
+					className="btn btn-danger btn-block rounded"
+					onClick={removeFromCart}
+				>
+					Remove from Cart
+				</button>
+			)
+		);
+	};
 
-    return(
-        <div className="card text-secondary mx-auto" style={{"width": "18rem"}}>
-            <ImageHelper product={product} />
-            <div className="card-body">
-                <h5 className="card-title">{cardTitle}</h5>
-                <p className="badge badge-secondary px-3 py-2">₹ {cardPrice}</p>                
-                <p className="card-text">{cardDescription}</p>
-                {showaddToCartButton(addToCartButton)}
-                {showremoveFromCartButton(removeFromCartButton)}
-                {performRedirect(redirect)}
-            </div>
-        </div>
-    );
-}
+	//Handle product Quantity
+	const incQuantity = () => {
+		let cart = loadCart();
+		cart.map((item) => {
+			if (item._id === product._id) {
+				item.quantity++;
+				updateCart(cart);
+				setReload(!reload);
+			}
+		});
+	};
+
+	//Handle product Quantity
+	const decQuantity = () => {
+		let cart = loadCart();
+		cart.map((item) => {
+			if (item._id === product._id) {
+				if (item.quantity > 1) {
+					item.quantity--;
+					updateCart(cart);
+				} else {
+					removeFromCart();
+				}
+				setReload(!reload);
+			}
+		});
+	};
+
+	const showQuantityButton = () => {
+		return product.quantity ? (
+			<div className="mb-2">
+				<p className="m-1">Quantity</p>
+				{product.quantity > 0 ? (
+					<button
+						onClick={decQuantity}
+						className="btn btn-danger mx-2 px-2 rounded"
+						style={{ fontSize: "0.5rem" }}
+					>
+						-
+					</button>
+				) : (
+					""
+				)}
+				<p style={{ display: "inline" }}>{product.quantity}</p>
+				<button
+					onClick={incQuantity}
+					className="btn btn-success mx-2 px-2 rounded"
+					style={{ fontSize: "0.5rem" }}
+				>
+					+
+				</button>
+			</div>
+		) : (
+			""
+		);
+	};
+
+	return (
+		<div className="card text-secondary mx-auto" style={{ width: "18rem" }}>
+			<ImageHelper product={product} />
+			<div className="card-body">
+				<h5 className="card-title">{cardTitle}</h5>
+				<p className="badge badge-secondary px-3 py-2">₹ {cardPrice}</p>
+				<p className="card-text">{cardDescription}</p>
+				{product.quantity
+					? showQuantityButton()
+					: showaddToCartButton(addToCartButton)}
+				{showremoveFromCartButton(removeFromCartButton)}
+				{performRedirect(redirect)}
+			</div>
+		</div>
+	);
+};
 
 export default Card;
