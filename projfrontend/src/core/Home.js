a92778cc8c5aa1aecfd67d0f840ba407cc25e870
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-13 19:00:31 +0530

    Bug-fix: Home Page crashing on first load
    Bug Cause: At first load, there is no 'cart' boject present in the localStorge which is required to load the quantity of products in cart and show it in the home page.
    Solution: running 'createCart' method at the load of Home page adds an empty cart object in localStorage if not present.

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -3,11 +3,12 @@ import "../styles.css";
 import Base from "../core/Base";
 import Card from "./Card";
 import { getProducts } from "./helper/coreapicalls"
-import { getQuantityFromCart } from "./helper/cartHelper";
+import { createCart, getQuantityFromCart } from "./helper/cartHelper";
 
 const Home = () => {
 	const [products, setProducts] = useState([]);
 	const [error, setError] = useState(false);
+
 	
 	const loadAllProducts = () => {
 		return getProducts()
@@ -28,6 +29,7 @@ const Home = () => {
 
 	return (
 		<Base title="Home Page" description="Welcome to the Tshirt Store">
+				{createCart()}
                 <h1 className="text-white mx-auto mb-5">All T-Shirts</h1>
 				<div className="row">
 					{products.map((product, index) => {
