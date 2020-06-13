95a1e85ed9525bb8a156ab8a3e02dd7b8cfbbced
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-13 22:48:37 +0530

    Added a method to get all orders from the backend

--- projfrontend/src/core/helper/orderHelper.js
+++ projfrontend/src/core/helper/orderHelper.js
@@ -13,3 +13,16 @@ export const createOrder = (userId, token, orderData) => {
     .then(response => response.json())
     .catch(err => console.log(err));
 }
+
+export const getAllOrders = (userId, token) => {
+    return fetch(`${API}/order/all/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+    })
+    .then(response => response.json())
+    .catch(err => console.log(err));
+}
