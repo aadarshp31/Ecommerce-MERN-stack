7e5119e694aed3c6d94f1304517427e7d5c85d2e
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-15 19:09:11 +0530

    Feature: Made the navigation bar for the website fixed.

--- projfrontend/src/core/Menu.js
+++ projfrontend/src/core/Menu.js
@@ -12,7 +12,7 @@ const currentTab = (history, path) => {
 };
 
 const Menu = (history) => (
-	<div>
+	<div style={{position: "fixed", width: "100vw", zIndex: "2"}}>
 		<ul className="nav nav-expand bg-info p-2 font-weight-bold">
 			<li className="nav-item">
 				<Link style={currentTab(history, "/")} className="nav-link" to="/">
