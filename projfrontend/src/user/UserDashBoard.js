d1fbeab02507153d95562b4b5bce109130095889
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-17 20:16:28 +0530

    Feature: Added UI for the 'User Dashboard' to handle all user info.

--- projfrontend/src/user/UserDashBoard.js
+++ projfrontend/src/user/UserDashBoard.js
@@ -1,9 +1,72 @@
 import React from "react";
 import Base from "../core/Base";
+import { Link } from "react-router-dom";
+import { isAuthenticated } from "../auth/helper";
+
+const { user } = isAuthenticated();
+
+const adminLeftSide = () => {
+	return(
+		<div className="card">
+			<h4 className="card-header bg-info text-white">User Navigation</h4>
+			<ul className="list-group">
+				<li className="list-group-item">
+					<Link to="/user/user-info" className="nav-link text-secondary">
+						Manage User Details
+					</Link>
+				</li>
+				<li className="list-group-item">
+					<Link to="/user/Orders" className="nav-link text-secondary">
+						Manage Orders
+					</Link>
+				</li>
+				<li className="list-group-item">
+					<Link to="/user/password-reset" className="nav-link text-secondary">
+						Change Password
+					</Link>
+				</li>
+				<li className="list-group-item">
+					<Link to="/user/email-update" className="nav-link text-secondary">
+						Change Email
+					</Link>
+				</li>
+				<li className="list-group-item">
+					<Link to="/user/orders" className="nav-link text-secondary">
+						Manage Orders
+					</Link>
+				</li>
+			</ul>
+		</div>
+	);
+}
+
+const adminRightSide = () => {
+	return(
+		<div className="card mb-4">
+			<h4 className="card-header bg-info text-white">User Information</h4>
+			<ul className="list-group text-secondary">
+				<li className="list-group-item">
+					<span className="badge badge-success mr-2">Name:</span> <span style={{textTransform: "capitalize"}}>{user.name + " " + user.lastname}</span>
+				</li>
+				<li className="list-group-item">
+					<span className="badge badge-success mr-2">Email:</span><span style={{textTransform: "lowercase"}}>{user.email}</span> 
+				</li>
+				<li className="list-group-item">
+					<span className="badge badge-success mr-2">Address:</span>PlaceHolder Address 
+				</li>
+			</ul>
+		</div>
+	);
+}
 
 const UserDashboard = () => {
 	return (
-		<Base title="User Dashboard Page" description="Welcome to User Dashboard Page"></Base>
+		<Base title="User Dashboard Page" description="Welcome to User Dashboard Page" className="container bg-white p-4 rounded">
+			<div className="row">
+				<div className="col-3">{adminLeftSide()}</div>
+				<div className="col-9">{adminRightSide()}</div>
+			</div>
+		</Base>
 	);
 };
 
