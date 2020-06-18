d67cbdba2233cf6cdd7a3ac88140ac4923784337
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-18 20:23:57 +0530

    Feature: Manage User Info Details
    Added 'preload' method to preload the user profile info by using 'getUser' method.
    Also added states to maintain status of the page and userInfo.

--- projfrontend/src/user/ManageUserInfo.js
+++ projfrontend/src/user/ManageUserInfo.js
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Base from "../core/Base";
 import { Link } from "react-router-dom";
+import { getUser } from "./helper/userapicalls";
+import { isAuthenticated } from "../auth/helper";
 
 const ManageUserInfo = () => {
+	const [userProfile, setUserProfile] = useState("");
+	const [status, setStatus] = useState({
+		loading: false,
+		error: false,
+		success: false,
+	});
+	const { user, token } = isAuthenticated();
+
+	const preload = () => {
+		setStatus({ loading: true });
+		getUser(user._id, token).then((data) => {
+			if (data.error) {
+				setStatus({ loading: false, error: data.error });
+			} else {
+				setUserProfile(data);
+				setStatus({ loading: true });
+			}
+		});
+	};
+
+	useEffect(() => {
+		preload();
+	}, []);
+
 	return (
 		<Base
 			title="Manage User Details Page"
 			description="Welcome to User Details Page"
 			className="container bg-white p-4 rounded"
 		>
-            {<Link className="btn btn-info rounded" to={`/user/dashboard`}>
-				<span>User Dashboard</span>
-			</Link>}
-        </Base>
+			{
+				<Link className="btn btn-info rounded" to={`/user/dashboard`}>
+					<span>User Dashboard</span>
+				</Link>
+			}
+		</Base>
 	);
 };
 
