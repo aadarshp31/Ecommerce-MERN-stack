8e4373654f5d8f7dee93355b4243093f76ca29d1
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-23 16:43:29 +0530

    Created a helper method 'getUserOrders'.
    This method helps to get all orders for the signed in user.

--- projfrontend/src/core/helper/orderHelper.js
+++ projfrontend/src/core/helper/orderHelper.js
@@ -1,28 +1,41 @@
 import { API } from "../../backend";
 
 export const createOrder = (userId, token, orderData) => {
-    return fetch(`${API}/order/create/${userId}`, {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify({ order: orderData })
-    })
-    .then(response => response.json())
-    .catch(err => console.log(err));
-}
+	return fetch(`${API}/order/create/${userId}`, {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${token}`,
+		},
+		body: JSON.stringify({ order: orderData }),
+	})
+		.then((response) => response.json())
+		.catch((err) => console.log(err));
+};
 
 export const getAllOrders = (userId, token) => {
-    return fetch(`${API}/order/all/${userId}`, {
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        },
-    })
-    .then(response => response.json())
-    .catch(err => console.log(err));
-}
+	return fetch(`${API}/order/all/${userId}`, {
+		method: "GET",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${token}`,
+		},
+	})
+		.then((response) => response.json())
+		.catch((err) => console.log(err));
+};
+
+export const getUserOrders = (userId, token) => {
+	return fetch(`${API}/orders/${userId}`, {
+		method: "GET",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${token}`,
+		},
+	})
+		.then((response) => response.json())
+		.catch((err) => console.log(err));
+};
