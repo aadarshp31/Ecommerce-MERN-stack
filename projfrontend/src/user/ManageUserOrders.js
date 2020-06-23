2d02e1426e7b663cb3391bd60d3523063cb9b99b
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-23 17:57:55 +0530

    Added Collapsable cards 'Accordion' fo each order.
    Each Order will have it's unique Collapsable card.
    The products purchased in the order will be displayed in the body of the collapsed card.

--- projfrontend/src/user/ManageUserOrders.js
+++ projfrontend/src/user/ManageUserOrders.js
@@ -20,7 +20,7 @@ const ManageUserOrders = () => {
 					setStatus({ ...status, loading: false, error: data.error });
 				} else {
 					setStatus({ ...status, loading: false });
-                    setOrders(data);
+					setOrders(data);
 				}
 			})
 			.catch((err) => {
@@ -43,6 +43,68 @@ const ManageUserOrders = () => {
 					<span>User Dashboard</span>
 				</Link>
 			}
+
+			<div className="accordion" id="accordionExample">
+				{orders.map((order, index) => {
+					return (
+						<div className="card" key={index}>
+							<div className="card-header" id="headingOne">
+								<h2 className="mb-0">
+									<button
+										className="btn btn-link btn-block text-left collapsed"
+										type="button"
+										data-toggle="collapse"
+										data-target={"#" + "colapse" + index}
+										aria-expanded="false"
+										aria-controls={"colapse" + index}
+									>
+										Collapsible Group Item #1
+									</button>
+								</h2>
+							</div>
+							<div
+								id={"colapse" + index}
+								className="collapse"
+								aria-labelledby="headingOne"
+								data-parent="#accordionExample"
+							>
+								<div className="card-body">
+									<table className="table table-striped">
+										<thead>
+											<tr>
+												<th scope="col">#</th>
+												<th scope="col">First</th>
+												<th scope="col">Last</th>
+												<th scope="col">Handle</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<th scope="row">1</th>
+												<td>Mark</td>
+												<td>Otto</td>
+												<td>@mdo</td>
+											</tr>
+											<tr>
+												<th scope="row">2</th>
+												<td>Jacob</td>
+												<td>Thornton</td>
+												<td>@fat</td>
+											</tr>
+											<tr>
+												<th scope="row">3</th>
+												<td>Larry</td>
+												<td>the Bird</td>
+												<td>@twitter</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					);
+				})}
+			</div>
 		</Base>
 	);
 };
