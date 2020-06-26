855fcb55782e9502760121913f02afa4dc327679
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-26 22:39:12 +0530

    Created a backend helper method called 'updatePassword'.
    This method communicates with the backend api to update the user's password via a protected route.

--- projfrontend/src/user/helper/userapicalls.js
+++ projfrontend/src/user/helper/userapicalls.js
@@ -1,29 +1,43 @@
-import { API } from "../../backend"
+import { API } from "../../backend";
 
 //Get User Profile
 export const getUser = (userId, token) => {
-    return fetch(`${API}/user/${userId}`, {
-        method: "GET",
-        headers: {
-            Accept: "applicaion/json",
-            "Content-Type": "applicaion/json",
-            Authorization: `Bearer ${token}`
-        }
-    })
-    .then(response => response.json())
-    .catch(err => console.log(err));
-}
+	return fetch(`${API}/user/${userId}`, {
+		method: "GET",
+		headers: {
+			Accept: "applicaion/json",
+			"Content-Type": "applicaion/json",
+			Authorization: `Bearer ${token}`,
+		},
+	})
+		.then((response) => response.json())
+		.catch((err) => console.log(err));
+};
 
 export const updateUser = (userId, token, userInfo) => {
-    return fetch(`${API}/user/${userId}`, {
-        method: "PUT",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify(userInfo)
-    })
-    .then(response => response.json())
-    .catch(err => console.log(err));
-}
+	return fetch(`${API}/user/${userId}`, {
+		method: "PUT",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${token}`,
+		},
+		body: JSON.stringify(userInfo),
+	})
+		.then((response) => response.json())
+		.catch((err) => console.log(err));
+};
+
+export const updatePassword = (userId, token, data) => {
+	return fetch(`${API}/user/password-reset/${userId}`, {
+		method: "PUT",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${token}`,
+		},
+		body: JSON.stringify(data),
+	})
+		.then((response) => response.json())
+		.catch((err) => err.json());
+};
