64ca7e12e9cbaf4f7cfbcbe4e2b7e7248df6ac81
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-26 22:37:04 +0530

    Added the 'updateUserPassword' method to a protected backend api route.
    Also used 'authorizeChange' middleware to authorise user to update his password.

--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const { check } = require("express-validator")
 
-const { getUserById, getUser, updateUser, userPurchaseList } = require("../controllers/user")
+const { getUserById, getUser, updateUser, updateUserPassword, userPurchaseList } = require("../controllers/user")
 const { isSignedIn, isAuthenticated, isAdmin, authorizeChange } = require("../controllers/auth")
 
 //Params middleware
@@ -11,9 +11,15 @@ router.param("userId", getUserById);
 //Protected routes
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 
+//Update Name of the user
 router.put("/user/:userId", [
     check("name").isLength({ min: 2 }).withMessage("First Name must be atleast 2 characters long")
 ], isSignedIn, isAuthenticated, authorizeChange, updateUser);
+
+//Change Password
+router.put("/user/password-reset/:userId", [
+    check("password").isLength({ min: 6 }).withMessage("First Name must be atleast 2 characters long")
+], isSignedIn, isAuthenticated, authorizeChange, updateUserPassword);
 
 router.get("/orders/user/:userId", isSignedIn, isAuthenticated, userPurchaseList)
 
