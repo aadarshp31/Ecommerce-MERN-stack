9dd0d6913b7826e0bd2cd02e72109eb7dd46f676
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-26 20:33:04 +0530

    Feature: Added ResetPassword form to the component.
    Added validation to check for password match and length of the password.

--- projfrontend/src/user/PasswordReset.js
+++ projfrontend/src/user/PasswordReset.js
@@ -1,9 +1,89 @@
-import React from "react";
+import React, {useState} from "react";
 import Base from "../core/Base";
 import { Link } from "react-router-dom";
 
 const PasswordReset = () => {
+    //React Hooks
+    const [ passwordObject, setPasswordObject] = useState({
+        newPass1: "",
+        newPass2: "",
+        oldPass: ""
+    })
+    const [ status, setStatus ] = useState({
+        loading: false,
+        error: "",
+        disableControls: false
+    })
 
+    //Destructuring
+    const { newPass1, newPass2, oldPass } = passwordObject;
+    const { loading, error } = status;
+
+    const handleChange = (name) => (event) => {
+        setPasswordObject({...passwordObject , [name]: event.target.value});
+    }
+
+    const validation = () => {
+        if ((newPass1 === newPass2) && newPass1.length >= 6) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+
+    const resetPasswordForm = () => {
+        return(
+            <form>
+            <h6 className={validation() ? "text-success" : "text-warning"}>Password must be atlease 6 digits in length</h6>
+            <h6 className={validation() ? "text-success" : "text-warning"}>New Password fields should match</h6>
+            <div className="input-group mb-3">
+				<div className="input-group-prepend">
+					<label className="input-group-text">New Password</label>
+				</div>
+				<input
+					type="password"
+					className={`form-control ${validation() ? `border-success` : `border-warning`}`}
+					placeholder="New Password"
+					value={newPass1}
+					onChange={handleChange("newPass1")}
+				/>
+			</div>
+            <div className="input-group mb-3">
+				<div className="input-group-prepend">
+					<label className="input-group-text">Retype New Password</label>
+				</div>
+				<input
+					type="password"
+					className={`form-control ${validation() ? `border-success` : `border-warning`}`}
+					placeholder="Retype New Password"
+					value={newPass2}
+					onChange={handleChange("newPass2")}
+				/>
+			</div>
+            <h6 className="text-info">Enter your current password to authorize this update</h6>
+			<div className="input-group mb-3">
+				<div className="input-group-prepend">
+					<label className="input-group-text">Password</label>
+				</div>
+				<input
+					type="password"
+					className="form-control"
+					placeholder="Password"
+					value={oldPass}
+					onChange={handleChange("oldPass")}
+					disabled={(newPass1 !== newPass2) || !newPass1 || !newPass2}
+				/>
+			</div>
+			<button
+				className="btn btn-info rounded"
+				disabled={(newPass1 !== newPass2) || !newPass1 || !newPass2 || !oldPass}
+			>
+				Update Info
+			</button>
+		</form>
+        );
+    }
 
 	return (
 		<Base
@@ -14,6 +94,7 @@ const PasswordReset = () => {
             <Link className="btn btn-info rounded mb-4" to="/user/dashboard">
                 User Dashboard
             </Link>
+            {resetPasswordForm()}
         </Base>
 	);
 };
