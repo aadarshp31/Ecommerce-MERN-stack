6fed6e34a1b40b071491520138f3f2654fad269d
aadarshp31 <adarshpandey.in@gmail.com>
2020-07-07 13:26:09 +0530

    Feature: Made the homescreen product cards responsive using flexbox.

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "../styles.css";
 import Base from "../core/Base";
 import Card from "./Card";
-import { getProducts } from "./helper/coreapicalls"
+import { getProducts } from "./helper/coreapicalls";
 import { createCart, getQuantityFromCart } from "./helper/cartHelper";
 
 const Home = () => {
@@ -10,38 +10,41 @@ const Home = () => {
 	const [error, setError] = useState(false);
 	const [reload, setReload] = useState(false);
 
-	
 	const loadAllProducts = () => {
 		return getProducts()
-		.then(data => {
-			if(data.error){
-				setError(data.error);
-			} else {
-				setProducts(data);
-			}
-		})
-		.catch(err => console.log(err));
-	}
+			.then((data) => {
+				if (data.error) {
+					setError(data.error);
+				} else {
+					setProducts(data);
+				}
+			})
+			.catch((err) => console.log(err));
+	};
 
 	useEffect(() => {
 		loadAllProducts();
-	}, [])
-
+	}, []);
 
 	return (
 		<Base title="Home Page" description="Welcome to the Tshirt Store">
-				{createCart()}
-                <h1 className="text-white mx-auto mb-5">All T-Shirts</h1>
-				<div className="row">
-					{products.map((product, index) => {
-						return(
-							<div key={index} className="col-4 mb-4">
-								<Card product={product} products={products} setReload={setReload} reload={reload} />
-							</div>
-						);
-					})}
-				</div>
-				{getQuantityFromCart(products)}
+			{createCart()}
+			<h1 className="text-white mx-auto mb-5">All T-Shirts</h1>
+			<div className="container p-1 d-flex flex-wrap justify-content-start">
+				{products.map((product, index) => {
+					return (
+						<div key={index} className="m-2">
+							<Card
+								product={product}
+								products={products}
+								setReload={setReload}
+								reload={reload}
+							/>
+						</div>
+					);
+				})}
+			</div>
+			{getQuantityFromCart(products)}
 		</Base>
 	);
 };
